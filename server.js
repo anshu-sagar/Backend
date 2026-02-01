34e8b9fa6714a72700f8fbed8df9b6bd84c0343c
anshu-sagar <anshu790611@gmail.com>
2026-02-01 20:27:22 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+const authRoutes = require("./src/routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is live 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
